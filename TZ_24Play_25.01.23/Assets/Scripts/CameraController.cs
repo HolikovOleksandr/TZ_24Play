a715fb76ac7cf03cf31bac6321652b72547bea85
Holikov Oleksandr <sanyagolikov97@gmail.com>
2023-01-25 17:54:49 +0200

    Created following camera to player with offset script

--- TZ_24Play_25.01.23/Assets/Scripts/CameraController.cs
+++ TZ_24Play_25.01.23/Assets/Scripts/CameraController.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Header("Player")]
+    [SerializeField] Transform _player;
     
+    [Header("Position Offset")]
+    [SerializeField] float _xPositionOffset;
+    [SerializeField] float _yPositionOffset;
+    [SerializeField] float _zPositionOffset;
+    
+    [Header("Rotation Offset")]
+    [SerializeField] float _xRotationOffset;
+    [SerializeField] float _yRotationOffset;
+
+    private void Update()
+    {        
+        transform.position = new Vector3
+        (
+            _player.position.x + _xPositionOffset, // Not happy with it
+            _player.position.y + _yPositionOffset,
+            _player.position.z + _zPositionOffset
+        ); 
+
+        Vector3 rotationOffset = new Vector3
+        (
+            _player.rotation.x + _xRotationOffset,
+            _player.rotation.y + _yRotationOffset
+        );    
+
+        transform.rotation = Quaternion.Euler(rotationOffset);
+    }
 }
